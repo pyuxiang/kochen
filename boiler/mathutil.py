df658ba799c399ffc2704b8169c1fff734cf725b
pyuxiang <pehyuxiang@gmail.com>
2022-10-08 11:57:05 +0800

    Add averaging utilities and devices.

--- boiler/mathutil.py
+++ boiler/mathutil.py
@@ -25,3 +25,101 @@ def cubicinterp(new_xs, old_xs, old_ys):
     """
     spfn = scipy.interpolate.splrep(old_xs, old_ys)
     return np.array([scipy.interpolate.splev(x, spfn) for x in new_xs])
+
+
+def smooth(xs: np.ndarray, window: int = 1):
+    """Simple smoothening by averaging fixed number of data points.
+    
+    Contexts:
+        Useful in cases where data is noisy, and there are many data points
+        from high resolution sampling. Originally from QT5201S HW2 when
+        cleaning up datapoints imported from nanosecond-resolution oscilloscope
+        over a range spanning tens of microseconds.
+    
+    Examples:
+        Clean up noisy/excessive data:
+
+        >>> xs = np.arange(1000)*np.pi/100
+        >>> ys = np.sin(xs)
+        >>> xs = smooth(xs, window=20)
+        >>> ys = smooth(ys, window=20)  # same window for consistency with xs
+    """
+    result = []
+    for i in range(xs.size//window):
+        result.append(np.mean(xs[window*i:window*(i+1)]))
+    return np.array(result)
+
+def bin(xx, yy: np.ndarray, start: float, end: float, n: int):
+    """Perform smoothening by averaging over x-valued bins.
+
+    Contexts:
+        Useful when x-data is not monotonically increasing, such as in a loop.
+        Binning can be performed to subsequently do partial derivatives over x-axis.
+    
+    Examples:
+        >>> xs, ys = bin(voltages, charges, -10, 10, 51)  # 51 points between -10V to 10V
+    """
+    xs = np.linspace(start, end, n)
+    ys = []
+    for i in range(1, xs.size):
+        s = xs[i-1]; e = xs[i]
+        _ = [y for i, y in enumerate(yy) if s <= xx[i] < e]
+        ys.append(np.mean(_))
+    xs += (xs[1]-xs[0])/2  # put in center of bin
+    # print(xs, ys)
+    return list(xs)[:-1], ys
+
+def pairfilter(pred, *xs):
+    """Perform group-based filtering based on filtering predicate.
+    
+    Args:
+        pred: Predicate for filtering, must have same number of arguments
+              as number of datarows supplied.
+        xs: Variable number of datarows for filtering.
+    
+    Contexts:
+        Originally from QT5201S homework 2, when calculations of instantaneous
+        gradient yielded spurious results near endpoints. Similarly used to
+        isolate features near zero-point, to calculate a mean value.
+    
+    Note:
+        This can probably be more efficiently implemented using numpy
+        row-operation conventions.
+    
+    Examples:
+        >>> x1 = np.linspace(0, 1, 101)
+        >>> x2 = np.linspace(1, 2, 100)
+        >>> pred = lambda x,y: (x > 0.5) and (y < 1.7)
+        >>> _ = pairfilter(pred, x1, x2)
+    """
+    data = list(zip(*xs))  # group individual data points
+    return list(zip(*[x for (*x,) in data if pred(*x)]))
+
+def manual_integration(ts, xs):
+    """
+    
+    Context:
+        Riemann summation for discrete integration. Dynamic programming.
+    """
+    width = ts[1] - ts[0]
+    xs = [(x*width) for x in xs]  # update values to area
+    integral = [xs[0]]  # boundary condition
+    for i in range(1, len(xs)):
+        integral.append(integral[-1] + xs[i])
+    return integral
+
+def manual_derivative(xs, ys):
+    """
+    
+    Context:
+        Instantaneous derivative.
+
+    Note:
+        numpy probably has something more straightforward, can replace with
+        function while substituting the theory here.
+    """
+    derivatives = []
+    for i in range(1, len(ys)):
+        derivatives.append((ys[i]-ys[i-1])/(xs[i]-xs[i-1]))
+    xs = xs[1:]
+    return xs, derivatives
--- boiler/plotutil.py
+++ boiler/plotutil.py
@@ -1,3 +1,6 @@
+# Do not import this module unless explicitly specified.
+# Mainly a book of recipes.
+
 def import_mpl_presentation():
     """Prepares matplotlib params for presentations.
 
@@ -10,3 +13,33 @@ def import_mpl_presentation():
     import matplotlib.pyplot as plt
     plt.rcParams.update({'axes.titlesize': 'x-large',
                         'axes.labelsize': 'x-large'})
+
+def create_parasitic_axes():
+    """Creates a secondary axis for overlaying correlated plots.
+
+    This method is most efficient, compared to the multitude of outdated
+    syntax in the official documentation examples.
+    """
+    import matplotlib.pyplot as plt
+
+    # Example data
+    xs = [0,1,2,3,4]
+    ys = [1,2,3,1,5]
+    zs = [0.01, 0.02, 0.01, -0.12, 0.03]
+
+    # Regular plotting as per usual
+    fig, ax = plt.subplots(figsize=(10,5))
+    plt.plot(xs, ys, label="$V_{in}$")
+    plt.xlabel("Time (ms)")
+    plt.ylabel("Voltage (V)")
+    plt.legend()
+
+    # Starts here: Create another axis sharing x-axis
+    ax2 = ax.twinx()
+    plt.sca(ax2)  # easiest to set as current axis to continue 'plt' syntax
+    plt.plot(xs, zs, c="tab:green", label="$I_{out}$")
+    plt.ylabel("Current (A)")
+    plt.legend()
+
+    plt.tight_layout()
+    plt.show()
