48a98bb36224ef1c169d7f86d4444d645e82be8d
pyuxiang <pehyuxiang@gmail.com>
2023-05-03 23:48:11 +0800

    Add convenience function for term coloring

---
+++ boiler/recipes/ansi.py
@@ -0,0 +1,56 @@
+#/usr/bin/env python3
+# Justin, 2023-05-03
+# Experiments with cross-platform ANSI colors
+#
+# 4-bit color support only, using ANSI escapes, i.e.
+#   Colors: BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE (+ RESET)
+#     - Extended: LIGHT{{color}}_EX 
+#   Styles: DIM, NORMAL, BRIGHT (+ RESET_ALL)
+#   Positioning: UP, DOWN, FORWARD, BACK, POS
+#
+# Note this may conflict with accessibility requirements, e.g.
+# VSCode may change the colors to achieve a minimum contrast ratio, see:
+# https://github.com/microsoft/vscode/issues/146168#issuecomment-1080762526
+
+import colorama
+colorama.just_fix_windows_console()
+
+def s(text, fg=None, bg=None, style=None, up=0):
+    """Returns text with ANSI wrappers for each line.
+    
+    Special note on newlines, where lines are broken up to apply
+    formatting on individual lines, excluding the newline character.
+
+    Position of start of print can be controlled using the 'up' arg.
+
+    Usage:
+        >>> print(s("hello\nworld", fg="red", style="dim"))
+        hello
+        world
+    """
+    # Construct formatting
+    fmt = ""
+    for c, cls in zip((fg, bg), (colorama.Fore, colorama.Back)):
+        if c:
+            c = c.upper()
+            if c.startswith("LIGHT"):
+                c += "_EX"
+            fmt += getattr(cls, c)
+    if style:
+        fmt += getattr(colorama.Style, style.upper())
+
+    # Force clear lines
+    fmt = colorama.ansi.clear_line() + fmt
+    
+    # Break by individual lines to apply formatting
+    lines = text.split("\n")
+    num_lines = len(lines)
+    lines = [f"{fmt}{line}{colorama.Style.RESET_ALL}" for line in lines]
+    text = "\n".join(lines)
+
+    # Apply move and restore position
+    # Assuming Cursor.DOWN will stop at bottom of current terminal printing
+    # Non-positive numbers are treated strangely.
+    if up > 0:
+        text = colorama.Cursor.UP(up) + text + colorama.Cursor.DOWN(up)
+    return text
