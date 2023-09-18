3ef28dd7e1f168ab72cfd9b240cae33b12e7253f
pyuxiang <pehyuxiang@gmail.com>
2023-09-18 12:29:50 +0800

    Bump version to 0.0.8

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ requirements_dev = [
 
 setuptools.setup(
     name="boiler",
-    version="0.0.7",
+    version="0.0.8",
     description="A compilation of boilerplate code",
     url="https://pyuxiang.com",
     author="Justin",
