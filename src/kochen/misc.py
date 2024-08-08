aa9b4481240288a7288c7b58481d4676e38938c5
pyuxiang <pehyuxiang@gmail.com>
2024-08-08 09:54:59 +0800

    Add PyYAML include method

--- src/kochen/misc.py
+++ src/kochen/misc.py
@@ -1,3 +1,9 @@
+import json
+import os
+
+import yaml
+from typing import Any, IO
+
 # Honestly no idea where this came from, but I'll leave it here until dissected...
 
 ########################################
@@ -48,3 +54,33 @@ def time():
         "from __main__ import combine, a, b",
         number=10,
     ))
+    
+
+
+class Loader(yaml.SafeLoader):
+    """YAML Loader with `!include` constructor.
+    
+    References:
+    	[1]: Source, <https://gist.github.com/joshbode/569627ced3076931b02f>
+    """
+    def __init__(self, stream: IO) -> None:
+        try:
+            self._root = os.path.split(stream.name)[0]
+        except AttributeError:
+            self._root = os.path.curdir
+
+        super().__init__(stream)
+
+
+def construct_include(loader: Loader, node: yaml.Node) -> Any:
+    filename = os.path.abspath(os.path.join(loader._root, loader.construct_scalar(node)))
+    extension = os.path.splitext(filename)[1].lstrip('.')
+    with open(filename, 'r') as f:
+        if extension in ('yaml', 'yml'):
+            return yaml.load(f, Loader)
+        elif extension in ('json', ):
+            return json.load(f)
+        else:
+            return ''.join(f.readlines())
+
+yaml.add_constructor('!include', construct_include, Loader)
