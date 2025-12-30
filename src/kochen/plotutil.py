6ad46f017105a457db21b69e91aab04e7b06451e
pyuxiang <pehyuxiang@gmail.com>
2025-12-30 13:45:15 +0800

    fix: invoke tick setter before tick label setter

--- src/kochen/plotutil.py
+++ src/kochen/plotutil.py
@@ -8,5 +8,7 @@ import matplotlib.dates as mdates
 def set_angled_dtimes(ax: maxes.Axes, angle=30, format="%-d %b, %-I%P"):
     dformat = mdates.DateFormatter(format)  # e.g. "7 Jun, 4am"
     ax.xaxis.set_major_formatter(dformat)
+    ticks = ax.get_xticks()  # pyright: ignore[reportCallIssue]
+    ax.set_xticks(ticks)  # pyright: ignore[reportCallIssue]
     labels = ax.get_xticklabels()  # pyright: ignore[reportCallIssue]
     ax.set_xticklabels(labels, rotation=angle, ha="right", rotation_mode="anchor")  # pyright: ignore[reportCallIssue]
